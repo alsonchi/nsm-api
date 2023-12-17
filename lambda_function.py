629187fff52dd209803a56fb4bb2d047b1836593
AlsonChi <alson.chi@auau.io>
2023-12-17 23:34:33 +0800

    UPDATE CODE

--- lambda_function.py
+++ lambda_function.py
@@ -14,5 +14,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps("Test GitHub Actions Deployment"),
+        'body': json.dumps(user)
     }
