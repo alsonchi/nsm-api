cd05c9a43bfa29c7c3b7e8d3afc884323c165441
Alson Chi <alson@digisalad.cool>
2023-12-20 14:56:19 +0800

    update function

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,10 @@
 import json
-import auth
+import jwt
+import boto3
 
 def lambda_handler(event, context):
     
-    user = auth.auth(event['headers']);
+    user = auth.auth(event['headers'])
     
     if user is None:
         return {
@@ -16,3 +17,34 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(user)
     }
+
+def auth(headers):
+    if "Authorization" not in headers:
+        return None
+        
+    token = (headers["Authorization"]).replace("Bearer ", "")
+    
+    #jwt decode
+    payload = jwt.decode(token, "secret", algorithms=["HS256"])
+        
+    return payload
+    
+    username = "alson"
+    
+    #check user token
+    dynamodb = boto3.resource("dynamodb", region_name="ap-southeast-1")
+    table = dynamodb.Table("nsm-user")
+    
+    #find user
+    user = table.query(
+        KeyConditions={
+            "user_name": {"AttributeValueList": [username], "ComparisonOperator": "EQ"}
+        }
+    )
+    
+    #check token
+    if user["Items"][0]["token"] != token:
+        return None
+    
+    return user["Items"][0]
+    
