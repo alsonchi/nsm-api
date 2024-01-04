e44a136705d88fa758dbecf4d4a100f6c5b263c7
Alson Chi <alson@digisalad.cool>
2024-01-04 16:44:05 +0800

    add get profile api

---
+++ lambda_nsm_profile.py
@@ -0,0 +1,35 @@
+import json
+import auth
+
+def lambda_handler(event, context):
+    user = auth.auth(event['headers'])
+    
+    if user is None:
+        return {
+            'statusCode': 401,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({"code": "unauthorized", "message": "Unauthorized"}),
+        }
+    
+    resource = {
+        'uuid' : user["Items"][0].get('uuid'),
+        'user_name': user["Items"][0].get('user_name'),
+        'display_name' : user["Items"][0].get('display_name'),
+        'avatar' : user["Items"][0].get('avatar'),
+        'status': user["Items"][0].get('status'),
+    }
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps({resource})
+    }
+    
